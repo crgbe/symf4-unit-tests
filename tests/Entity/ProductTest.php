f9f9d7b9f69597c33ec3b9183471b5b4f75bdf24
crgbe <credo.gbededji@gmail.com>
2020-01-24 17:31:11 +0100

    Utilisation de l'annotation dataProvider

--- tests/Entity/ProductTest.php
+++ tests/Entity/ProductTest.php
@@ -9,25 +9,38 @@ use PHPUnit\Framework\TestCase;
 
 class ProductTest extends TestCase
 {
-    public function testComputeTVAFoodProduct(){
-        $product = new Product('Product', 'food', 20);
-        $result = $product->computeTVA();
+    /**
+     * @dataProvider getPricesAndExpectedProductTVAForFood
+     * @param float $price
+     * @param float $expectedTVA
+     */
+    public function testComputeTVAFoodProduct($price, $expectedTVA){
+        $product = new Product('Product', Product::FOOD_PRODUCT, $price);
 
-        $this->assertSame(1.1, $result);
+        $this->assertSame($expectedTVA, $product->computeTVA());
     }
 
-    public function testComputeTVAOtherProduct(){
-        $product = new Product('Product', 'others', 10);
-        $result = $product->computeTVA();
+//    public function testComputeTVAOtherProduct(){
+//        $product = new Product('Product', 'others', 10);
+//        $result = $product->computeTVA();
+//
+//        $this->assertSame(1.96, $result);
+//    }
+//
+//    public function testComputeTVANotNegative(){
+//        $product = new Product('Product', 'any-food', -12);
+//
+//        $this->expectException('LogicException');
+//
+//        $product->computeTVA();
+//    }
 
-        $this->assertSame(1.96, $result);
-    }
-
-    public function testComputeTVANotNegative(){
-        $product = new Product('Product', 'any-food', -12);
-
-        $this->expectException('LogicException');
-
-        $product->computeTVA();
+    public function getPricesAndExpectedProductTVAForFood(){
+        return [
+            [10, 0.55],
+            [20, 1.1],
+            [15, 0.825],
+            [45, 2.475],
+        ];
     }
 }
